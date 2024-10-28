20c3ed08048aa87750b2643e69320461b8037738
Yash Pal <pyash3137@gmail.com>
2024-10-28 11:32:05 +0530

    initial commit set up of app

---
+++ todo/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <div className='flex justify-center mt-4b bg-blue-900 p-4'>
+        <header>
+            <h1 className=" text-white	font-style: italic font-bold text-2xl">TODO</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
